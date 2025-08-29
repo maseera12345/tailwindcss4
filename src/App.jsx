389177d4be4b16bdfa6ab73096fe5e5b062bbfc3
maseera12345 <maseeramalik42@gmail.com>
2025-08-28 22:46:06 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,224 @@
+
+
+
+
+import "./App.css";
+import React from "react";
+
+
+function App() {
+  return (
+    <div className=" text-white min-h-screen">
+
+   
+
+      
+      <nav className="w-full bg-[#333335]  flex justify-between items-center ">
+        <img
+          src="https://tse4.mm.bing.net/th/id/OIP.9Kj-bJbHatXfQ2EM_1diPgHaE8?pid=Api&P=0&h=220"
+          alt="SFlix Logo"
+          className="h-12 ml-4"
+        />
+        <div className="flex space-x-4 mr-4">
+          <p className="hover:text-blue-500 ">Home</p>
+          <p className="hover:text-blue-500 ">Movies</p>
+          <p className="hover:text-blue-500 ">TV Shows</p>
+          <p className="hover:text-blue-500 ">Top IMDb</p>
+        </div>
+      </nav>
+
+
+
+      <section className="w-full min-h-screen flex flex-col justify-start items-center pt-32">
+       
+
+
+        <div className="flex flex-col items-center mb-6">
+          <img
+            src="https://tse4.mm.bing.net/th/id/OIP.9Kj-bJbHatXfQ2EM_1diPgHaE8?pid=Api&P=0&h=220"
+            alt="SFlix Logo"
+            className="h-60 w-60 mb-2"
+          />
+          <h6 className="text-xl font-bold mb-4">SFlix</h6>
+        </div>
+
+        
+        
+        <div className=" bg-white text-black flex justify-center items-center mb-4">
+          <input type="text"
+            placeholder="Enter keyword"
+            className="p-2 w-80  "
+          />
+          <button  className="bg-blue-600 text-white px-4 py-2 ">
+            Search
+          </button>
+          </div>
+
+         
+        <div>
+        <button className="bg-green-400 inline text-white mr-2 mb-6">
+         <a href="http://www.whatsapp.com">Whatsapp</a>
+        </button>
+        <button className="bg-blue-500 inline text-white mr-2 mb-6">
+         <a href="http://m.facebook.com">Facebook</a>
+        </button>
+        
+        <button className="bg-black inline text-white mr-2 mb-6">
+        <a href="http://x.com.">Twitter</a>
+        </button>      
+          <button className="bg-blue-500 inline text-white  mr-2 mb-6">
+        <a href="http://telegram.org">Telegram</a>
+        
+        </button>
+</div>
+          
+
+      
+
+       
+        <div className="mb-4">
+          <button className="bg-blue-500 px-6 py-2  hover:scale-110 transition">
+            View Full Site
+          </button>
+        </div>
+
+        
+
+      
+<section className="w-full px-4 md:px-20 py-8 text-white ">
+ 
+  <h2 className="text-2xl font-bold mb-4 text-center">Watch Movies Online Free</h2>
+
+  
+  <p className="mb-4">
+    SFlix is one of the best free sites to watch movies online for free in 2021. 
+    With no registration or payment required, you can binge-watch tens of thousands 
+    of movies and TV shows in HD quality seamlessly and safely. New titles are updated 
+    on the site daily to make sure fun never ends on SFlix. Should you not find your 
+    movie of interest, simply make a request and we will scour the Internet to provide 
+    you the content you have been longing for.
+  </p>
+
+  <p className="mb-4">
+    SFlix is where you can get exclusive premium features at the cost of nothing. 
+    We provide HD quality, superb streaming capabilities, safe and private source 
+    links, and the ad-free feature completely for free!
+  </p>
+
+  <h2 className="text-xl font-bold mb-3">What is SFlix?</h2>
+  <p className="mb-4">
+    SFlix is a newly-created site that allows users to watch and download movies 
+    and TV shows online in HD quality for free. SFlix’s purpose is to become a free 
+    alternative to Netflix so movie enthusiasts can enjoy all the features that the 
+    giant streaming service offers without having to pay a dime. SFlix is created in 
+    order for movie fans to watch movies safely and seamlessly no matter what their 
+    financial situation is. We might have a long way to go to reach our goal, but we 
+    believe, with your support, it can be achieved soon.
+  </p>
+
+  <h2 className="text-xl font-bold mb-3">Is SFlix or 123Movies Better for streaming movies and shows?</h2>
+  <p className="mb-4">
+    123Movies is no doubt the most popular free movie site ever. At peak, 123movies 
+    welcomed 98 million users a month. To many movie fans, whenever they think of free 
+    movie streaming, the domain 123movies.to pops up. Unfortunately, the site was shut 
+    down in 2018 and since then, we haven’t heard any official news about a comeback 
+    from the team. In fact, multiple sites have been created to copy 123movies and 
+    they might be ill-intentioned. Visiting such sites might cause you troubles because 
+    they are all filled with malicious ads. For your complete security, you should stay 
+    with SFlix instead. Hackers have no chances to access your device or gain your 
+    private information on SFlix as we are free from ads and do not require any registration.
+  </p>
+
+  <h2 className="text-xl font-bold mb-3">Is It Illegal to Use SFlix?</h2>
+  <p className="mb-4">
+    We are currently accessible all over the world. SFlix is not technically a legal 
+    site, however, using SFlix for free online movie streaming is not illegal. According 
+    to copyright attorneys, you will only be convicted for a jail term or subject to 
+    criminal or civil charges when you share or download pirated content. If you insist 
+    on downloading videos to watch offline later, use a reliable VPN and proceed at your own risk.
+  </p>
+
+  <h2 className="text-xl font-bold mb-3">Is SFlix safe?</h2>
+  <p className="mb-4">
+    SFlix is a new site so it is understandable if our security is questioned. There 
+    has not been much feedback about the site’s safety for references, but you should 
+    not be worried about your safety here. SFlix is ad-free; therefore, you are safe 
+    from viruses, trojans, malware, and other malicious computer programs. SFlix does 
+    not require you to reveal any private information for a signup; therefore, you are 
+    also safe from information leakage and identity theft. Unlike other free movie sites, 
+    you do not need to use a VPN, anti-virus program, and ADBlock extension while 
+    binge-watching movies and TV shows on SFlix. Your movie night should be spent 
+    without any worries. And with these features, we are confident that it will be 
+    the best movie night you have ever had.
+  </p>
+
+  <ul className="list-disc ml-5 mb-4">
+    <li>An enormous collection of movies and TV shows with multiple subtitles.</li>
+    <li>HD Resolution (720p). Adjustable.</li>
+    <li>Private and safe streaming source.</li>
+    <li>Fast load times and seamless streaming feature.</li>
+    <li>Fast and constant content updates.</li>
+    <li>Optimized UI & UX.</li>
+    <li>Mobile-friendly and Chromecast supported.</li>
+    <li>No ads, pop ups, or commercials.</li>
+    <li>No account or registration needed.</li>
+    <li>Best customer service.</li>
+  </ul>
+
+  <p className="mb-4">
+    Are you struggling to look for free streaming sites that is the best alternative 
+    to SFlix? Or do your eyes hurt from watching blurry shows and movies? If your answer 
+    is yes, it’s time to check out this list of 9 top movies sites where you can watch 
+    movies in HD quality, with subtitles, and free of charge!
+  </p>
+
+  <h2 className="text-xl font-bold mb-3">
+    Top 9 Free Movie Streaming Sites</h2>
+  <ol className="list-decimal ml-5 mb-4">
+  <li className="mb-4">
+  <span className="font-bold">123Movies:</span>
+  <p>
+    This is arguably the most popular website for free streaming movies online. With its huge 
+    number of movies and shows, the site has reached a peak of 98 million users overall. You can 
+    almost find anything you want to watch here, if not, simply make a request and their staff 
+    will try their best to find it for you. No registration, up-to-date contents, HD quality, 
+    subtitles and even the option of surfing on more than one server, 123Movies HD is what 
+    everyone can ask for from a streaming site. Although 123Movies was shut down, there are 
+    rumors that it is still available on clone sites. It is only a VPN click away!
+  </p>
+</li>
+</ol>
+</section>
+
+
+       
+        <div className="mb-8">
+          <button className="bg-blue-500 px-6 py-2 rounded hover:scale-110 transition">
+            Go to SFlix
+          </button>
+        </div>
+      </section>
+
+     
+      <footer className="w-full bg-[#222] text-center py-6 text-white">
+  <p className="mb-2 inline mr-4  ">
+<a href="https://www.exness.global/?utm_source=partners&cid=we32hqmirt85cdbcj15rvmlm&ex_ol=1"className="mr-4 inline">Android</a>
+<a href="https://www.exness.global/?utm_source=partners&cid=we32hqmirt85cdbcj15rvmlm&ex_ol=1" className="mr-4 inline">Terms of Services</a>
+<a href="https://www.exness.global/?utm_source=partners&cid=we32hqmirt85cdbcj15rvmlm&ex_ol=1"className="mr-4 inline">Sitemap</a>
+<a href="https://www.exness.global/?utm_source=partners&cid=we32hqmirt85cdbcj15rvmlm&ex_ol=1" className="mr-4 inline">9Animie</a>
+</p>
+  <p className="mb-4">
+    SFlix is a Free Movies streaming site with zero ads. We let you watch movies online without 
+    having to register or paying, with over 10000 movies and TV-Series. You can also Download full 
+    movies from MoviesCloud and watch it later if you want.
+  </p>
+
+  <p>&copy; 2025 SFlix</p>
+</footer>
+
+    </div>
+
+  );
+}
+
+export default App;
